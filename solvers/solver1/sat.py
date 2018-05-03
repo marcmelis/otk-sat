4e74a0bde99387e09ba7034f99f305f9bfb73803
Roger Truchero <truchero.roger@gmail.com>
2018-05-04 01:51:56 +0200

    Added gsat solver with random variable selection

--- solvers/solver1/sat.py
+++ solvers/solver1/sat.py
@@ -24,9 +24,8 @@ def parse(filename):
     return clauses, int(n_vars)
 
 
-def random_variable_selection(formula):
-    counter = get_counter(formula)
-    return random.choice(counter.keys())
+def random_variable_selection(n_vars):
+    return random.randint(0, n_vars - 1)
 
 
 def get_counter(formula):
@@ -46,8 +45,6 @@ def check_solution(solution, formula):
         sl = map(int, clause)
         length = len(sl)
         for lit in sl:
-            print solution
-            print lit, solution[abs(lit) - 1]
             if lit == solution[abs(lit) - 1]:
                 break
             else:
@@ -57,39 +54,40 @@ def check_solution(solution, formula):
     return True
 
 
-"""def gsat(formula, assignment, n_vars):
-    max_flips = 10
-    max_tries = 1
-    for tries in range(1, max_tries):
-        A = initial_configuration(n_vars)
-        for flips in range(1, max_flips):
-            if A satisfies formula:
-                return A
-            # Selecció de variable
-            x = select-variable(A)
-            A = A with x flipped
-    return solution"""
+def gsat(formula, n_vars):
+    max_flips = 100000
+    max_tries = 100
+    while True:
+        assignment = initial_configuration(n_vars)
+        for tries in range(1, max_tries):
+            for flips in range(1, max_flips):
+                if check_solution(assignment, formula):
+                    return assignment
+                x = random_variable_selection(n_vars)
+                # print "Variable selected: ", x + 1, " - Solution: ", assignment
+                assignment[x] = -assignment[x]
+                # print "Solution Flipped: ", assignment
 
 
 def initial_configuration(n_vars):
     init_conf = list()
     for i in range(1, n_vars + 1):
-        init_conf.append(i)
+        prob = random.randint(0, 1)
+        if prob == 1:
+            init_conf.append(i)
+        else:
+            init_conf.append(-i)
     return init_conf
 
 
 def main():
     formula, n_vars = parse(sys.argv[1])
-    A = initial_configuration(n_vars)
-    print A
-    """solution = gsat(formula, [], n_vars)
-    print solution
+    solution = gsat(formula, n_vars)
     if check_solution(solution, formula):
         print 's SATISFIABLE'
         print 'v ' + ' '.join([str(x) for x in solution]) + ' 0'
     else:
         print 's UNSATISFIABLE'
-"""
 
 
 if __name__ == '__main__':
