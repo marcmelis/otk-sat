9790adf1d08a8e3ad85828f9e2cf51a2dd27566b
Marc Melis <markankaro2016@gmail.com>
2018-05-25 17:20:05 +0200

    Fixed license date

--- src/otk_sat.py
+++ src/otk_sat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #######################################################################
-# Copyright 2013 Marc Melis
+# Copyright 2018 Marc Melis
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
